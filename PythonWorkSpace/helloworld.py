a91dfccc6620de20496b26741b57adf0b5cdad3c
kaist33 <52246375+kaist33@users.noreply.github.com>
2020-11-20 00:59:18 +0900

    1

--- PythonWorkSpace/helloworld.py
+++ PythonWorkSpace/helloworld.py
@@ -22,6 +22,7 @@ animal = "강아지"
 age = 4
 hobby = "낮잠"
 is_adult = age >= 3
+
 print("우리집 " + animal + "의 이름은 연탄이예요")
 print("연탄이는 " + str(age) + "이며, " + hobby +"을 아주 좋아해요")
 print("연탄이는 어른일까요? " + str(is_adult))
---
+++ PythonWorkSpace/practice.py
@@ -0,0 +1,2 @@
+station1 = "사당"
+print(station1 + "행 열차가 들어오고 있습니다.")
