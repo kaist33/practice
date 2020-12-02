f8a569ac9492923450878701dc3c00d482e9aff1
kaist33 <52246375+kaist33@users.noreply.github.com>
2020-12-03 00:55:01 +0900

    1

--- rpm_basic_1_excel/3_cell.py
+++ rpm_basic_1_excel/3_cell.py
@@ -21,4 +21,14 @@ print(ws["A10"].value) # 값이 없을 땐 'none'을 출력
 print(ws.cell(row=1, column=1).value) # ws["A1"].value
 print(ws.cell(row=1, column=2).value) # ws["B1"].value
 
+c = ws.cell(column=3, row=1, value=10) # ws["C1"].value = 10
+print(c.value) # ws["C1"]
+
+from random import *
+
+# 반복문을 이용해서 랜덤 숫자 채우기
+for x in range(1,11): # 10개 row
+    for y in range(1,11): # 10 개 column
+        ws.cell(row=x, column=y, value=randint(0,100)) # 0~100 사이의 숫자
+
 wb.save("sample.xlsx")
