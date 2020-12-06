55c1977ffe4082b25b3d54152718b0975b6e5798
kaist33 <52246375+kaist33@users.noreply.github.com>
2020-12-06 23:38:36 +0900

    1

--- rpm_basic_1_excel/4_open_file.py
+++ rpm_basic_1_excel/4_open_file.py
@@ -5,5 +5,5 @@ ws = wb.active # 활성화된 Sheet
 # cell 데이터 불러오기
 for x in range(1,11):
     for y in range(1,11):
-        print (ws.cell(row=x, column=y).value, end=" ") # 1 2 3 4
+        print (ws.cell(row=x, column=y).value, end=" ") # 1 2 3 4 ..
     print()
